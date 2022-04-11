8d082afec3c45368f43263ea1d264858a2bacd8b
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-04-11 16:42:21 +0600

    initial firebase setup

---
+++ src/__steps.js
@@ -0,0 +1,8 @@
+
+/** hostings steps
+ * npm install -g firebase-tools
+ * firebase login
+ * firebase init
+ * firebase deploy
+ * 
+*/
---
+++ src/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth'
+
+const firebaseConfig = {
+     apiKey: "AIzaSyAeeionrV3oFQMmmn34UEmWqWGTc-ksRh0",
+     authDomain: "emah-john-simple.firebaseapp.com",
+     projectId: "emah-john-simple",
+     storageBucket: "emah-john-simple.appspot.com",
+     messagingSenderId: "781208526548",
+     appId: "1:781208526548:web:d55c5f15fb19f4de2ae541"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+export default auth;
