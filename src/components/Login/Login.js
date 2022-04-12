50e8d0f4beba49189401917e02e3c2a67e01851b
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-04-12 13:44:21 +0600

    signInWithEmailAndPassword createUserWithEmailAndPassword added

---
+++ src/components/Login/Login.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import './Login.css'
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+
+const Login = () => {
+     const [email, setEmail] = useState('');
+     const [password, setPassword] = useState('');
+     const navigate = useNavigate();
+     // const [signInWithEmailAndPassword, loading, user, error] = useSignInWithEmailAndPassword(auth);
+
+     const [
+          signInWithEmailAndPassword,
+          user,
+          loading,
+          error,
+     ] = useSignInWithEmailAndPassword(auth);
+     const handleEmailBlur = event => {
+          setEmail(event.target.value);
+     }
+     const handlePassBlur = event => {
+          setPassword(event.target.value);
+     }
+
+     if (user) {
+          navigate('/shop');
+     }
+
+     const hanleUserSignIn = event => {
+          event.preventDefault();
+          signInWithEmailAndPassword(email, password);
+     }
+
+     return (
+          <div className='form-container'>
+               <div>
+                    <h2 className='form-title'>Login</h2>
+                    <form onSubmit={hanleUserSignIn}>
+                         <div className="input-group">
+                              <label htmlFor="email">Email</label>
+                              <input onBlur={handleEmailBlur} type="email" name="email" id="" required />
+                         </div>
+                         <div className="input-group">
+                              <label htmlFor="password">Password</label>
+                              <input onBlur={handlePassBlur} type="password" name="password" id="" required />
+                         </div>
+                         <p style={{ color: 'red' }}>{error?.message}</p>
+                         {
+                              loading && <p>Loading...</p>
+                         }
+                         <input className='form-submit' type="submit" value="Login" />
+                    </form>
+                    <p className='form-link-container'>
+                         New to Ema-John?
+                         <Link className='form-link' to="/signup">Create New Account</Link>
+                    </p>
+                    <p className='or'>or</p>
+                    <p className='google-link'>
+                         <img src="https://cdn2.hubspot.net/hubfs/53/image8-2.jpg" alt="" />
+                         <p>Continue with Google</p>
+                    </p>
+               </div>
+          </div>
+     );
+};
+
+export default Login;
---
+++ src/components/Signup/Signup.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init'
+
+const Signup = () => {
+     const [email, setEmail] = useState('');
+     const [password, setPassword] = useState('');
+     const [confirmPassword, setConfirmPassword] = useState('');
+     const [error1, setError] = useState('');
+     const navigate = useNavigate('')
+     const [createUserWithEmailAndPassword, error, user] = useCreateUserWithEmailAndPassword(auth);
+
+
+     const handleEmailBlur = event => {
+          setEmail(event.target.value)
+     }
+     const handlePassBlur = event => {
+          setPassword(event.target.value)
+     }
+     const handleConfrimPassBlur = event => {
+          setConfirmPassword(event.target.value)
+     }
+
+     if (user) {
+          navigate('/shop')
+     }
+
+     const handleCreateUser = event => {
+          event.preventDefault()
+          if (password !== confirmPassword) {
+               setError("Your two passwords did't match")
+               return
+          }
+          if (password.length < 6) {
+               setError("Password must be 6 caracter")
+               return;
+          }
+
+          createUserWithEmailAndPassword(email, password);
+     }
+     return (
+          <div className='form-container'>
+               <div>
+                    <h2 className='form-title'>Sign up</h2>
+                    <form onSubmit={handleCreateUser}>
+                         <div className="input-group">
+                              <label htmlFor="email">Email</label>
+                              <input onBlur={handleEmailBlur} type="email" name="" id="" required />
+                         </div>
+                         <div className="input-group">
+                              <label htmlFor="password">Password</label>
+                              <input onBlur={handlePassBlur} type="password" name="password" id="" required />
+                         </div>
+                         <div className="input-group">
+                              <label htmlFor="password">Confirm Password</label>
+                              <input onBlur={handleConfrimPassBlur} type="password" name="confirm-password" id="" required />
+                         </div>
+
+                         <p style={{ color: "red" }}>{error1}</p>
+                         <input className='form-submit' type="submit" value="Sign up" />
+                    </form>
+                    <p className='form-link-container'>
+                         Already have an account?
+                         <Link className='form-link' to="/login">Create Login</Link>
+                    </p>
+                    <div className='or-container'>
+                         <p className='border'></p>
+                         <p className='or'>or</p>
+                         <p className='border'></p>
+                    </div>
+                    <p className='google-link'>
+                         <img src="https://cdn2.hubspot.net/hubfs/53/image8-2.jpg" alt="" />
+                         <p>Continue with Google</p>
+                    </p>
+               </div>
+          </div>
+     );
+};
+
+export default Signup;
